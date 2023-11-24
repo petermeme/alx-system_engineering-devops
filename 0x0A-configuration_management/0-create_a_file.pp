a979f22a85f7898c083f49599485bde2664e7eeb
petermeme <petermeme90@gmail.com>
2023-11-24 14:13:26 -0800

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/pup
+# This Puppet manifest creates a file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# create a manifest that kills a process named killmenow.
+exec { 'process kill killmenow':
+  path    => '/usr/bin/',
+  command => 'pkill -f killmenow',
+}
